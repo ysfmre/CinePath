7d0954ee6e108d0acd075e2257f897965b8903a6
Yusuf Emre Öner <yuxufemre@gmail.com>
2026-01-18 21:08:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ export const CONFIG = {
         messagingSenderId: "781626702698",
         appId: "1:781626702698:web:66ac7f1ce955cbf8f40313"
     },
-    GEMINI_API_KEY: "AIzaSyA44fa81BZCxab7XDE6i9cbbN4JC7e4SoA"
+    GEMINI_API_KEY: "AIzaSyCp7h9qNwZviNFf63ZWsuhUPIOY5a6tkVc"
 };
